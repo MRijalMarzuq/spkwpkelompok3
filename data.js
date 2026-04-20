f30b45e8004e2343332f2519150c7e0d85f7d25a
MRijalMarzuq <mrijalmarzuq@gmail.com>
2026-04-20 22:26:04 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,11 +3,10 @@ const templateData = [
     ["Bobot", 0.20, 0.20, 0.20, 0.10, 0.20, 0.10],
     ["Tipe", "cost", "benefit", "benefit", "cost", "benefit", "benefit"],
     ["SMA Negeri 1 Indramayu", 1000000, 5, 4, 8.7, 5, 16],
-    ["MAN 1 Indramayu", 1200000, 1, 1, 8.5, 3, 9],
+    ["SMA Negeri 2 Indramayu", 1200000, 1, 1, 8.5, 3, 9],
     ["SMA Negeri 1 Sindang", 2300000, 5, 5, 10.2, 5, 15],
-    ["SMA Negeri 1 Kerangkeng", 2000000, 4, 3, 31.5, 4, 10],
-    ["SMA Negeri 1 Anjatan", 1500000, 4, 4, 45, 4, 15],
+    ["SMA Negeri 1 Karangampel", 2000000, 4, 3, 31.5, 4, 10],
+    ["SMA Negeri 1 Santa Maria", 1500000, 4, 4, 45, 4, 15],
     ["SMA Negeri 1 Lohbener", 1000000, 3, 4, 1.3, 3, 15],
     ["SMA 1 Sliyeg", 1500000, 3, 3, 13.7, 3, 13],
-    ["Hasil/Benefit/Cost", 1000000, 5, 5, 1.3, 5, 16]
 ];
